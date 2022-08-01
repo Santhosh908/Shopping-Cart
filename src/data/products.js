5170e468b8b242e0edd1fdbc16a997596a82bbdc
Santhosh908 <santhoshyavd@gmail.com>
2022-08-01 12:02:04 +0530

    images

--- src/data/products.js
+++ src/data/products.js
@@ -6,7 +6,6 @@ const data = [
     currency: 'RS',
     image: 'images/01.jpg',
   },
-  
   {
     id: 2,
     name: 'Samsung 192Litres(Stabilizer Free)',
@@ -15,32 +14,53 @@ const data = [
     image: 'images/02.jpg',
   },
   {
-    id: 4,
-    name: 'Schlafsessel Rovigo',
-    price: 1599,
+    id: 3,
+    name: 'Preethi Excel Plus 117 1600-Watt Induction Cooktop(Black)',
+    price:  3496.00,
     currency: 'Rs',
-    image: 'images/04.jpg',
+    image: 'images/03.jpg',
   },
   {
     id: 6,
-    name: 'Sessel Little',
-    price: 119.99,
+    name: 'Tri Cylce for babies',
+    price: 2599.00,
     currency: 'Rs',
     image: 'images/06.jpg',
   },
   {
     id: 5,
-    name: 'Sessel Peacock',
-    price: 599.99,
+    name: 'OnePlus Nord 2 5G (Gray Sierra, 12GB RAM, 256GB Storage)',
+    price: 15999.00,
     currency: 'Rs',
     image: 'images/05.jpg',
   },
   {
-    id: 3,
-    name: 'Sessel Anna',
-    price: 1499,
+    id: 6,
+    name: 'Canon EOS-R Mirrorless Camera Body Free delivery',
+    price: 138999.00,
     currency: 'Rs',
-    image: 'images/03.jpg',
+    image: 'images/010.jpg',
+  },
+  {
+    id: 7,
+    name: 'Bajaj Victor VPR01 400mm Pedestal Fan (Blue)',
+    price: 555.00,
+    currency: 'Rs',
+    image: 'images/07.jpg',
+  },
+  {
+    id: 8,
+    name: 'Bajaj Victor VPR01 400mm Pedestal Fan (Blue)',
+    price: 555.00,
+    currency: 'Rs',
+    image: 'images/07.jpg',
+  },
+  {
+    id: 9,
+    name: 'SLIPSTREAM (52" SPAN, TEAK FINISH METAL BODY, TEAK FINISH ABS BLADES) CEILING FAN',
+    price: 21950.00 ,
+    currency: 'Rs',
+    image: 'images/09.jpg',
   }
 ];
 
